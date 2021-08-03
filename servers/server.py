7eb27669fde5d41960a2b07d0c711fff40c757b7
TechnoCat44 <jamesaqu@buffalo.edu>
2021-08-02 21:10:12 -0400

    first commit

---
+++ servers/server.py
@@ -0,0 +1,31 @@
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    '''
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    
+    '''
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        received_data = self.request.recv(1024).strip()
+        print(self.client_address[0] + "is sending data: ") #self.client_address gives us the info about the requester
+        print(received_data.decode()) # this turns the data into a string
+        print("\n\n")
+        
+        self.request.sendall("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 17\r\n\r\nWhat's up world!!".encode())
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = 8000
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+
+    server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler)
+    server.serve_forever()
+
